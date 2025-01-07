42a6c11261a8004c8e37cdd01d3e365e60c65d1d
Keisuke2162 <applabo.kei@gmail.com>
2025-01-08 01:01:28 +0900

    AccountをEntityに切り出し、Repositoryモジュール作成

--- Kei3MapAppPackage/Sources/Feature/Root/RootView.swift
+++ Kei3MapAppPackage/Sources/Feature/Root/RootView.swift
@@ -103,19 +103,3 @@ public struct RootView: View {
   }
 }
 
-public struct Account: Codable {
-  public var userID: String = ""
-  public let userName: String
-  public let userProfileImageURL: URL?
-
-  public init(userID: String, userName: String, userProfileImageURL: URL?) {
-    self.userID = userID
-    self.userName = userName
-    self.userProfileImageURL = userProfileImageURL
-  }
-
-  enum CodingKeys: String, CodingKey {
-    case userName = "name"
-    case userProfileImageURL = "thumbnailURL"
-  }
-}
