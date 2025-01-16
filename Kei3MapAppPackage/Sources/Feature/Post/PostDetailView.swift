5260abf5ee107a198fdc56fd80e16bc1dae27d64
Keisuke2162 <applabo.kei@gmail.com>
2025-01-17 04:49:49 +0900

    投稿の表示をモーダルに変更

--- Kei3MapAppPackage/Sources/Feature/Post/PostDetailView.swift
+++ Kei3MapAppPackage/Sources/Feature/Post/PostDetailView.swift
@@ -4,6 +4,8 @@ import Kingfisher
 
 public struct PostDetailView: View {
   let postItem: Post
+  let gradientView: LinearGradient = .init(gradient: Gradient(colors: [.red, .green, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+  @Environment(\.dismiss) var dismiss
 
   public init(postItem: Post) {
     self.postItem = postItem
@@ -11,24 +13,44 @@ public struct PostDetailView: View {
 
   public var body: some View {
     VStack {
-      RoundedRectangle(cornerRadius: 0)
-        .aspectRatio(1, contentMode: .fit)
-        .overlay {
-          KFImage(postItem.postImageURL)
-            .resizable()
-            .scaledToFill()
-        }
-        .clipShape(RoundedRectangle(cornerRadius: 4))
-        .padding(.top, 16)
-      Text(postItem.addressString)
-        .foregroundStyle(.gray)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.horizontal, 16)
-      Text(postItem.postText)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.horizontal, 16)
-        .padding(.top, 16)
       Spacer()
+      HStack(spacing: 8) {
+        RoundedRectangle(cornerRadius: 0)
+          .aspectRatio(1, contentMode: .fit)
+          .overlay {
+            KFImage(postItem.postImageURL)
+              .resizable()
+              .scaledToFill()
+          }
+          .frame(width: 80)
+          .clipShape(RoundedRectangle(cornerRadius: 4))
+        VStack(spacing: 4) {
+          Spacer()
+          Text(postItem.addressString)
+            .foregroundStyle(.gray)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 16)
+          Text(postItem.postText)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 16)
+            .padding(.top, 16)
+          Spacer()
+        }
+      }
+      .padding(.leading, 16)
+      
+      Button {
+        dismiss()
+      } label: {
+        Text("経路案内🚶")
+          .font(.title3.bold())
+          .tint(Color.black)
+          .padding(.horizontal, 32)
+          .padding(.vertical, 8)
+          .background(gradientView)
+      }
+      .clipShape(.rect(cornerRadius: 16))
     }
+    Spacer()
   }
 }
